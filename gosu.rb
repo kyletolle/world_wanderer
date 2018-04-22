0077c6bbbd0a30004cd28a6b304ea274718f57d6
Kyle Tolle <kyle@nullsix.com>
2018-04-21 18:45:08 -0700

    Add a player class and image

--- gosu.rb
+++ gosu.rb
@@ -18,4 +18,43 @@ class Tutorial < Gosu::Window
   end
 end
 
+class Player
+  def initialize
+    @image = Gosu::Image.new('starfighter.bmp')
+    @x = @y = @vel_x = @vel_y = @angle = 0.0
+    @score = 0
+  end
+
+  def warp(x, y)
+    @x, @y = x, y
+  end
+
+  def turn_left
+    @angle -= 4.5
+  end
+
+  def turn_right
+    @angle += 4.5
+  end
+
+  def accelerate
+    @vel_x += Gosu.offset_x(@angle, 0.5)
+    @vel_y += Gosy.offset_y(@angle, 0.5)
+  end
+
+  def move
+    @x += @vel_x
+    @y += @vel_y
+    @x %= 640
+    @y %= 480
+
+    @vel_x *= 0.95
+    @vel_y *= 0.95
+  end
+
+  def draw
+    @image.draw_rot(@x, @y, 1, @angle)
+  end
+end
+
 Tutorial.new.show
