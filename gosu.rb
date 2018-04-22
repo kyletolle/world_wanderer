dad2aef31d79f539f50184b1c5a173aa21a0344a
Kyle Tolle <kyle@nullsix.com>
2018-04-21 18:55:24 -0700

    Add a ZOrder module and Star class

--- gosu.rb
+++ gosu.rb
@@ -1,5 +1,9 @@
 require 'gosu'
 
+module ZOrder
+  BACKGROUND, STARS, PLAYER, UI = *0..3
+end
+
 class Tutorial < Gosu::Window
   def initialize
     super 640, 480
@@ -74,6 +78,26 @@ class Player
 
   def draw
     @image.draw_rot(@x, @y, 1, @angle)
+  end
+end
+
+class Star
+  attr_reader :x, :y
+
+  def initialize(animation)
+    @animation = animation
+    @color = Gosu::Color::BLACK.dup
+    @color.red = rand(256 - 40) + 40
+    @color.green = rand(256 - 40) + 40
+    @color.blue = rand(256 - 40) + 40
+    @x = rand * 640
+    @y = rand * 480
+  end
+
+  def draw
+    img = @animation[Gosu.milliseconds / 100 % @animation.size]
+    img.draw(@x - img.width / 2.0, @y - img.height / 2.0,
+             Zorder::STARS, 1, 1, @color, :add)
   end
 end
 
