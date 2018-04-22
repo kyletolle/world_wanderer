4989be6a7bad6b2545a0f0f66fc884e19171c16a
Kyle Tolle <kyle@nullsix.com>
2018-04-21 18:49:32 -0700

    Allow starfighter to move around

--- gosu.rb
+++ gosu.rb
@@ -6,15 +6,35 @@ class Tutorial < Gosu::Window
     self.caption = "Tutorial Game"
 
     @background_image = Gosu::Image.new('space.png')
+
+    @player = Player.new
+    @player.warp(320, 240)
   end
 
   def update
-    # ...
+    if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
+      @player.turn_left
+    end
+    if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
+      @player.turn_right
+    end
+    if Gosu.button_down? Gosu::KB_UP or Gosu::button_down? Gosu::GP_BUTTON_0
+      @player.accelerate
+    end
+    @player.move
   end
 
   def draw
+    @player.draw
     @background_image.draw(0, 0, 0)
-    # ...
+  end
+
+  def button_down(id)
+    if id == Gosu::KB_ESCAPE
+      close
+    else
+      super
+    end
   end
 end
 
@@ -39,7 +59,7 @@ class Player
 
   def accelerate
     @vel_x += Gosu.offset_x(@angle, 0.5)
-    @vel_y += Gosy.offset_y(@angle, 0.5)
+    @vel_y += Gosu.offset_y(@angle, 0.5)
   end
 
   def move
